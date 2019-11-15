190701eb4757a3d14764f6eb3d72cfe2793d149e
Rai Butera <rai@rbutera.com>
2019-11-15 21:03:53 +0000

    initial commit

---
+++ code/canvas.tsx
@@ -0,0 +1,11 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const Fe_search = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_HmMZx8363", {}, 28,28);
+export const Header = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_eEjnrigJp", {}, 375,60);
+export const IPhone_X_Dark = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_S1yqxglKu", {}, 375,812);
+export const Mailing_List_CTA = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_GBS7kpxZZ", {}, 216,74);
+export const Search_FAB = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_tpuh_u4tI", {}, 36,36);
+export const Searchbox = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_N2KR9uKyp", {}, 362,52);
+export const Section_Header = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_CMZZ0BQ_0", {}, 281,83);
+export const That_s_Lit_Minimal_Logo = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_yoFYHJS3b", {}, 159,96);
