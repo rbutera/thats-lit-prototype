13b96445e6a30dbf75883667d40fa3efdce676ef
Rai Butera <rai@rbutera.com>
2019-12-02 14:54:14 +0000

    cleaned up ready for flip tweaks

--- code/canvas.tsx
+++ code/canvas.tsx
@@ -3,5 +3,3 @@ import { createDesignComponent, CanvasStore } from "framer"
 const canvas = CanvasStore.shared(); // CANVAS_DATA;
 export const Fe_search = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_HmMZx8363", {}, 28,28);
 export const IPhone_X_Dark = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_S1yqxglKu", {}, 375,812);
-export const Slant_Top_Background = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_HDTTtW2lb", {}, 375,28);
-export const Titlebar = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_ACZzQWuPz", {}, 375,229);
